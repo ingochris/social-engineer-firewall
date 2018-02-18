05e7f97b587f690037079b239595dbe4fe51ab77
Kyle1668 <kyleobrien01@comcast.net>
2018-02-18 06:51:07 -0800

    kyle - final

--- final/index.js
+++ final/index.js
@@ -1,25 +1,19 @@
-// var og_quer = req.body.Disambiguation.ChoiceData[0].FixedTranscription;
-// var patt = /(maiden\sname)|(first\spet)|(favorite\steacher)|(first\sschool)|(first\sjob)|(first\scar)|(favorite\sbook)|(favorite\sfood)|(\bcity\W+(?:\w+\W+)*?born\b)|(\bcity\W+(?:\w+\W+)*?birth\b)|(first\steacher)|(\bchildhood\W+(?:\w+\W+)*?friend\b)|(\bmeet\W+(?:\w+\W+)*?spouse\b)|(mascot)/gi;
-// var res = patt.exec(og_quer);
-// 	if(res == null) {
-//         	//loop listener
-//       } else {
-//         	alert('Warning! Possible Social Engineering Attack Detected!') // return true and the trigger word
-//       }
-
-
-//       //
-
-
 function checkText() {
     var query = JSON.parse($("#responseJSON").val()).Disambiguation.ChoiceData[0].Transcription;
-    var patt = /(maiden\sname)|(first\spet)|(favorite\steacher)|(first\sschool)|(first\sjob)|(first\scar)|(favorite\sbook)|(favorite\sfood)|(\bcity\W+(?:\w+\W+)*?born\b)|(\bcity\W+(?:\w+\W+)*?birth\b)|(first\steacher)|(\bchildhood\W+(?:\w+\W+)*?friend\b)|(\bmeet\W+(?:\w+\W+)*?spouse\b)|(mascot)/gi;
-    var res = patt.exec(query);
-    
-    if(res == null) {
-            //loop liste''ner
-            console.log("cool");
-      } else {
-        	alert('Warning! Possible Social Engineering Attack Detected!') // return true and the trigger word
-      }
+
+    if (query != "") {
+        var pattern = /(maiden\sname)|(first\spet)|(favorite\steacher)|(first\sschool)|(first\sjob)|(first\scar)|(favorite\sbook)|(favorite\sfood)|(\bcity\W+(?:\w+\W+)*?born\b)|(\bcity\W+(?:\w+\W+)*?birth\b)|(first\steacher)|(\bchildhood\W+(?:\w+\W+)*?friend\b)|(\bmeet\W+(?:\w+\W+)*?spouse\b)|(mascot)/gi;
+        var result = pattern.exec(query);
+        
+        if (!result) {
+                console.log("No attacks detected");
+          } else {
+                alert('Warning! Possible Social Engineering Attack Detected!') // return true and the trigger word
+          }
+    }
+   
 }
+
+// $("#responseJSON").change(function() {
+//     checkText();
+// });
